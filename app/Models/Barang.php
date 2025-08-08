3eab91170bc26014191e6fb3df30d2103fb4b165
nrhlizaoct9 <nurhalizaoct09@gmail.com>
2025-08-08 18:05:21 +0700

    Membuat model Barang

---
+++ app/Models/Barang.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Barang extends Model
+{
+    use HasFactory;
+
+    protected $table = 'barangs';
+
+    protected $fillable = [
+        'nama_barang',
+        'jenis_barang_id',
+        'stok',
+        'satuan',
+        'harga'
+    ];
+
+    /**
+     * Kolom yang harus disembunyikan dari array/JSON
+     */
+    protected $hidden = [
+        'created_at',
+        'updated_at'
+    ];
+
+    public function getHargaFormattedAttribute()
+    {
+        if (is_null($this->harga)) {
+            return '-';
+        }
+        return 'Rp ' . number_format($this->harga, 2, ',', '.');
+    }
+
+
+    public function jenisBarang()
+    {
+        return $this->belongsTo(JenisBarang::class);
+    }
+}
--- app/Models/JenisBarang.php
+++ app/Models/JenisBarang.php
@@ -9,37 +9,19 @@ class JenisBarang extends Model
 {
     use HasFactory;
 
-    /**
-     * Nama tabel yang terkait dengan model.
-     *
-     * @var string
-     */
     protected $table = 'jenis_barangs';
 
-    /**
-     * Kolom yang dapat diisi massal (mass assignable).
-     *
-     * @var array
-     */
     protected $fillable = [
         'nama_jenis'
     ];
 
-    /**
-     * Kolom yang harus disembunyikan dari array dan JSON.
-     *
-     * @var array
-     */
     protected $hidden = [
         'created_at',
         'updated_at'
     ];
 
-    /**
-     * Relasi ke model Barang (jika diperlukan)
-     */
-    // public function barangs()
-    // {
-    //     return $this->hasMany(Barang::class, 'jenis_barang_id');
-    // }
+    public function barangs()
+    {
+        return $this->hasMany(Barang::class, 'jenis_barang_id');
+    }
 }
